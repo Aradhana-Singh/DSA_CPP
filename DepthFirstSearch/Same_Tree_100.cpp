c6b01a8043e8009387aec7354fcfe0f788bded36
Aradhana Singh <aradhana971127@gmail.com>
2020-05-07 17:31:24 +0530

    commit

---
+++ DepthFirstSearch/Same_Tree_100.cpp
@@ -0,0 +1,24 @@
+/* Given two binary trees, write a function to check if they are the same or not.
+Two binary trees are considered the same if they are structurally identical and 
+the nodes have the same value. */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if( p == NULL && q == NULL) return true;
+        if( p == NULL || q == NULL) return false;
+        if( p->val != q->val) return false;
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+    }
+};
