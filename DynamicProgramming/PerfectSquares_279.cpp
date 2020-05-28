556a763740e0076a6b03c01d8003ba12d7198bc7
Aradhana Singh <aradhana971127@gmail.com>
2020-05-28 17:04:36 +0530

    commit

---
+++ DynamicProgramming/PerfectSquares_279.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        int dp[n+1];
+        dp[1] = 1;
+        dp[0] = 0;
+        for(int i =2 ; i<=n; i++){
+            dp[i] = INT_MAX;
+            for(int j = 1; j*j<=i; j++){
+                dp[i] = min(dp[i] , dp[i-(j*j)] + 1);
+            }
+        }
+        return dp[n];
+    }
+};
